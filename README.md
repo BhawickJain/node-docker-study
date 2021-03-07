ea835cff6215735c2ceaad8478acc4640674e6dc
BhawickJain <bhawick@outlook.com>
2021-03-07 15:49:50 +0000

    learned and created working examples of javascript / node parts, use app.js to run each of the modules

--- README.md
+++ README.md
@@ -11,6 +11,43 @@ URL: github.com/BhawickJain/node-docker-study
 Takes code from `server-docker-1` and build on it to further Node.js.
 
 `[x]` Write an example of a function body  
-`[ ]` How to import and create your own modules  
+`[x]` How to import and create your own modules  
+`[x]` Introduction to the Path Node API  
 
-There is a `window` object which has a number of methods: `setTimeout()`, `clearTimeout()`, `console.log`. The window is usually not declared.
+There is a `global` object which has a number of methods: `setTimeout()`, `clearTimeout()`, `console.log`. The window is usually not declared. For browsers, there is the equivalent `window` object.
+
+You avoid using global scopes as names and functions scoped globally will be overidden by the last declaration, which is confusing and leads to having to understand the entire code based.
+
+Modules are core to Node.js, each file is a module and you need to `export` them to make them available to other places.
+
+use jshint to catch issues in your code. You need to install jshint globally as bash doesn't seem to find any of the projects installed by npm during the image build.
+`[ ]` Installed JS Hint and tried it  
+
+Each module is wrapped in an immediately invoked function expressed (IFFY) which is basically a function but wrapped as `(/* function*/)()` enabling no assignment to a variable. It means you can invoke something immediately without reveal the variables inside the function. Examples function and arrow function:
+
+```
+(function() {
+    console.log("iffy function");
+})()
+
+const normalFunc = function() {
+    console.log("normal function");
+}
+
+(() => {
+    console.log("iffy arrow function");
+})()
+
+const arrowFunc = () {
+    console.log("arrow function");
+}
+
+```
+
+Each module is wrapped in a IFFY with special variables like so:
+
+```
+(function (exports, require, module, __filename, __dirname) {
+  /* module contents you've written */
+})();
+```
--- app/app.js
+++ app/app.js
@@ -1,22 +1,41 @@
-// Example of a function body
-const sayHello = (name) => {
-  return "Hello " + name;
-};
-
-// a global object
-console.log(sayHello("Bhawick"));
-
-global.console.log(
-  "Console.Log method belongs to the global object which is a globally available within Node.js"
-);
-
-let name_var = "Bhawick";
-
-// declared functions and variables are scoped to app.js
-console.log(
-  "Variables and Function I define here are not added to the global object hence `name` = " +
-    global.name_var
-);
-
 // information about the app.js module
-console.log(module);
+// console.log(module);
+
+// When importing a module, node will first see if it is a module in the
+// project if not found, it will then look for installed packages via npm
+
+// import logger module, keep them at the top of the file in real dev
+// same directory, load it as a constant
+// const logger = require("./logger");
+
+// prevents dumb things like:
+// logger = 1;
+// try changing the const to let and uncommenting the above
+
+// move to parent direction and find logger
+// kinda like cd .. in bash
+// const logger = require("../logger");
+
+// shows that logger contains a exports, which in this case is a function log.
+// when url is exposed as endPoint in the logger.js you can see it too
+// console.log(logger);
+// logger.log("this-is-a-log-mesage-timestamp");
+
+// IFFY Function are invoked immediately whilst other
+// functions remain hidden as they have not be exported.
+// every module is wrapped in an IFFY function.
+// require("./iffyFunctions");
+
+// const broken = require("./brokenModule");
+// broken.functionExport();
+// also note calling functionExport without () returns nothing
+// broken.functionExport;
+
+// Example of what global objects are.
+// require("./globalObjects");
+
+// Example of how Scheduling Timers are used
+// const schedulingTimers = require("./schedulingTimers");
+
+// Example of pathNodeAPI usage
+const pathNodeAPI = require("./pathNodeAPI");
